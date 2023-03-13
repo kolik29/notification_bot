c019ff96d7933e523cf64925bb86b4cd36202fee
root <root@1313562-cp56784.tw1.ru>
2023-03-13 21:21:49 +0300

    comments

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ app.use(bodyParser.json());
 const token = '6231043770:AAEqdLDUfoCVcdrR644yx63qcXV5BbgaJ_U';
 const bot = new telegramBot(token, { polling: true });
 
-let chat_ids = [];
+let chat_ids = []; // список id подключенных чатов
 
-app.post('/send_user_data', (req, res) => {
-    console.log(req.body);
+app.post('/send_user_data', (req, res) => { // Хук срабатывает по запросу на http://example.ru/send_user_data
+    console.log(req.body); // выводить в консоль тело запроса
 
-    chat_ids.forEach(id => {
-        bot.sendMessage(id, `
+    chat_ids.forEach(id => { 
+        // и каждому подключенному юзеру отправляет сообщение
+        bot.sendMessage(id, ` 
 Новый юзер
 Имя: ${req.body.name}
 Почта: ${req.body.email}
@@ -22,12 +23,12 @@ app.post('/send_user_data', (req, res) => {
     })
 })
 
-bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, 'Успешный успех');
+bot.onText(/\/start/, (msg) => { // при первом выполнении команды /start
+    bot.sendMessage(msg.chat.id, 'Успешный успех'); // подверждение, что этот скрипт работает
 
-    chat_ids.push(msg.chat.id);
+    chat_ids.push(msg.chat.id); // добавляет id чата в массив
 })
 
 app.listen(3001, () => {
-    console.log(`Server running at http://185.178.45.117:3001/`)
+    console.log(`Server running at http://185.178.45.117:3001/`);
 })
